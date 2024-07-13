0be25fdeb0d892468e79122be8acc55ae23699b9
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-13 16:11:10 +0100

    updating with bottomnav

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:multiva/screen/productList.dart';
+import 'package:multiva/nav.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProductScreen(), 
+      home: BottomNav(), 
     );
   }
 }
--- lib/nav.dart
+++ lib/nav.dart
@@ -1,11 +1,12 @@
-import 'package:Essentials/screens/shop/categories/category.dart';
+
 
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:multiva/screen/cart.dart';
 import 'package:multiva/screen/productList.dart';
 import 'package:multiva/screen/profile.dart';
 
-;
+
 
 
 
@@ -66,47 +67,28 @@ class _BottomNavState extends State<BottomNav> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  IconButton(
-                    icon: const Icon(Iconsax.home),
-                    color: _selectedIndex == 0 ? Colors.grey : Colors.white,
-                    onPressed: () => _onItemTapped(0),
-                  ),
+                 
                   IconButton(
                     icon: const Icon(Iconsax.category),
                     color: _selectedIndex == 1 ? Colors.grey : Colors.white,
-                    onPressed: () => _onItemTapped(1),
+                    onPressed: () => _onItemTapped(0),
                   ),
                   const SizedBox(width: 40), // Space for FAB
                   IconButton(
-                    icon: const Icon(Iconsax.bookmark),
+                    icon: const Icon(Iconsax.shopping_bag),
                     color: _selectedIndex == 3 ? Colors.grey : Colors.white,
-                    onPressed: () => _onItemTapped(3),
+                    onPressed: () => _onItemTapped(1),
                   ),
                   IconButton(
                     icon: const Icon(Iconsax.user),
                     color: _selectedIndex == 4 ? Colors.grey : Colors.white,
-                    onPressed: () => _onItemTapped(4),
+                    onPressed: () => _onItemTapped(2),
                   ),
                 ],
               ),
             ),
           ),
-          Positioned(
-            bottom: 50.0,
-            left: MediaQuery.of(context).size.width / 2 - 30,
-            child: FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  _selectedIndex = 2; // Navigate to Cart page
-                  _pageController.jumpToPage(2);
-                });
-              },
-              child: const Icon(Icons.shopping_cart),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    );
+  ]    )  
+   );
   }
 }
