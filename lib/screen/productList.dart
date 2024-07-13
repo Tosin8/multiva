0e17d5cc3e93195f1ca8c0cfc234a70d0b6abc0c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-13 19:05:51 +0100

    Update productList.dart

--- lib/screen/productList.dart
+++ lib/screen/productList.dart
@@ -9,112 +9,96 @@ class ProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return Scaffold(
       backgroundColor: Colors.grey.shade300,
-   appBar: PreferredSize(
-  
-  preferredSize: const Size.fromHeight(50),
- child: 
- AppBar(
-  elevation: 0,
-  
-  backgroundColor: Colors.grey.shade300,
-  automaticallyImplyLeading: false,
-  title: const Text('Product List',), 
-  
-  centerTitle: true,
- 
-  )),
-  body: Padding(
-    padding: const EdgeInsets.only(left: 8, 
-    right: 8), 
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        const SizedBox(height: 20,),
-        Stack( 
-          children: [
-          Container(
-            
-            height: 250,
-            width: MediaQuery.of(context).size.width,
-           child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.asset('assets/images/bill.jpg', fit: BoxFit.cover,), 
-          ), 
-          
-           ),
-            Positioned( 
-            top: 60, 
-            left: 20,
-             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Premium Sounds', style: TextStyle(fontSize: 20, color: Colors.white, 
-                fontWeight: FontWeight.bold),
-                ), 
-                 const Text('Premium Savings', style: TextStyle(fontSize: 20, color: Colors.white, 
-                fontWeight: FontWeight.bold),
-                ),
-                 const Text('Limited Offer, hurry up and \nget yours now', style: TextStyle(fontSize: 16, color: Colors.white, 
-              ),
-                ),
-              ].animate(interval: 400.ms).fade(duration: 500.ms).slide(),
-             ),
-           )
-          ], 
-        ),    
-    
-    
-    // Title Category .
-    const SizedBox(height: 20,), 
-    const Text('Tech Gadget', style: headerStyle), 
-    const SizedBox(height: 10,), 
-ListView.builder(
-  scrollDirection: Axis.horizontal,
-  itemCount: techGadgets.length,
-  shrinkWrap: true,
-  itemBuilder: (context, index) =>  Container(
-      height: 400, width: 200,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.asset(techGadgets[index].imageUrl, height: 200, width:200,  fit: BoxFit.cover,),
-          ), 
-          const SizedBox(height: 10,), 
-           Text(techGadgets[index].name, 
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),), 
-          const SizedBox(height: 5,), 
-         Text(techGadgets[index].description, 
-        style: TextStyle(fontSize: 16),
-        maxLines: 2,
-        softWrap:true,
-        overflow: TextOverflow.ellipsis,), 
-        const SizedBox(height: 5,), 
-           Text(techGadgets[index].price, 
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),),
-          const SizedBox(height: 5,), 
-          Container(
-            height: 40, width: 150,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              color: Colors.pink,
-            ),
-            child: const Align(child: Text('Add to Cart',
-             style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),)),
-          ), 
-        ],
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.grey.shade300,
+          automaticallyImplyLeading: false,
+          title: const Text('Product List'),
+          centerTitle: true,
+        ),
       ),
-    ), 
-), 
-    // Product List
-   
-    ],
-    ),
-  ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 8, right: 8),
+          child: Column(
+            children: [
+              const SizedBox(height: 20,),
+              // Billboard
+              Stack(
+                children: [
+                  Container(
+                    height: 250,
+                    width: MediaQuery.of(context).size.width,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.asset('assets/images/bill.jpg', fit: BoxFit.cover,),
+                    ),
+                  ),
+                  Positioned(
+                    top: 60,
+                    left: 20,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Premium Sounds', style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold)),
+                        const Text('Premium Savings', style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold)),
+                        const Text('Limited Offer, hurry up and \nget yours now', style: TextStyle(fontSize: 16, color: Colors.white)),
+                      ].animate(interval: 400.ms).fade(duration: 500.ms).slide(),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20,),
+              // Title Category
+              const Text('Tech Gadget', style: headerStyle),
+              const SizedBox(height: 10,),
+              SizedBox(
+                height: 400,  // Adjust the height to fit your content
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: techGadgets.length,
+                  itemBuilder: (context, index) => Container(
+                    width: 200,
+                    margin: const EdgeInsets.only(right: 8),  // Add some spacing between items
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(techGadgets[index].imageUrl, height: 200, width: 200, fit: BoxFit.cover,),
+                        ),
+                        const SizedBox(height: 10,),
+                        Text(techGadgets[index].name, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+                        const SizedBox(height: 5,),
+                        Text(techGadgets[index].description, style: const TextStyle(fontSize: 16), maxLines: 2, softWrap: true, overflow: TextOverflow.ellipsis,),
+                        const SizedBox(height: 5,),
+                        Text(techGadgets[index].price, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+                        const SizedBox(height: 5,),
+                        Container(
+                          height: 40,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Colors.pink,
+                          ),
+                          child: const Align(
+                            child: Text('Add to Cart', style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              // Product List
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
