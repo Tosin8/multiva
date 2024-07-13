7992d5815dd5e7b63acd4253d0c1dc6f5e756338
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-13 14:56:33 +0100

    adding animation to bill

--- lib/productList.dart
+++ lib/productList.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:iconsax/iconsax.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -22,24 +23,52 @@ class ProductScreen extends StatelessWidget {
   centerTitle: true,
  
   )),
-  body: Column(
-    children: [
-      const SizedBox(height: 20,),
-      Container(
-        
-        height: 40,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          image: const DecorationImage(
-            image: AssetImage('assets/images/bill.jpg'),
-          ) ),    
-  ), 
-
-  // Title Category .
-
-  const Text('Tech Gadget')
-  ],
+  body: Padding(
+    padding: const EdgeInsets.only(left: 2, right: 2), 
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const SizedBox(height: 20,),
+        Stack( 
+          children: [
+          Container(
+            
+            height: 250,
+            width: MediaQuery.of(context).size.width,
+           child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset('assets/images/bill.jpg', fit: BoxFit.cover,), 
+          ), 
+          
+           ),
+            Positioned( 
+            top: 60, 
+            left: 20,
+             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Premium Sounds', style: TextStyle(fontSize: 20, color: Colors.white, 
+                fontWeight: FontWeight.bold),
+                ).animate().fadeIn(duration: 1000.ms), 
+                 const Text('Premium Savings', style: TextStyle(fontSize: 20, color: Colors.white, 
+                fontWeight: FontWeight.bold),
+                ),
+                 const Text('Limited Offer, hurry up and \nget yours now', style: TextStyle(fontSize: 16, color: Colors.white, 
+              ),
+                ),
+              ],
+             ),
+           )
+          ], 
+        ),    
+    
+    
+    // Title Category .
+    
+    const Text('Tech Gadget', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
+    ],
+    ),
   ),
     );
   }
